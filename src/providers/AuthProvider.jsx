bd0ffc53805ad4bd865a29c6cd47d01506d11453
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-31 16:22:01 +0600

    deploy firebase

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -22,8 +22,6 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
 
-  console.log('current user -->', user);
-
   const createUser = (email, password) => {
     setLoading(true)
     return createUserWithEmailAndPassword(auth, email, password)
