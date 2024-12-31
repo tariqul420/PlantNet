65162c1e45752f7ecc2313adfb4a8baa9d5f9867
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-31 12:17:17 +0600

    add user in mongodb

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -22,6 +22,8 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
 
+  console.log('current user -->', user?.email);
+
   const createUser = (email, password) => {
     setLoading(true)
     return createUserWithEmailAndPassword(auth, email, password)
@@ -52,9 +54,18 @@ const AuthProvider = ({ children }) => {
   // onAuthStateChange
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async currentUser => {
-      console.log('CurrentUser-->', currentUser?.email)
       if (currentUser?.email) {
         setUser(currentUser)
+
+        const userData = {
+          name: currentUser?.displayName,
+          image: currentUser?.photoURL,
+          email: currentUser?.email,
+        }
+
+
+        // Save user in mongodb
+        await axios.post(`${import.meta.env.VITE_API_URL}/users`, userData)
 
         // Get JWT token
         await axios.post(
